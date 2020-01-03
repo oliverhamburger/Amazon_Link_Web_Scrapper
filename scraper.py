1ee995199d1f9616d017a8c887e855bc29a2db64
ohamburger22 <ohamburger22@wooster.edu>
2020-01-02 23:22:43 -0500

    Updated page link to another page with a price

--- scraper.py
+++ scraper.py
@@ -94,7 +94,7 @@ pages = []
 #create amazon.com pages and add them to the list
 headphones = Page('https://www.amazon.com/Powerbeats-Pro-Totally-Wireless-Earphones/dp/B07R5QD598/ref=sr_1_3?crid=1P13WRASZMXBL&keywords=beats+workout+earbuds&qid=1576214123&sprefix=beats+work%2Caps%2C147&sr=8-3', 50)
 pages.append(headphones)
-computer = Page('https://www.amazon.com/Apple-MacBook-15-inch-512GB-Storage/dp/B07S58MHXF/ref=pd_rhf_dp_s_vtp_ses_clicks_shared_1_3/146-3009192-6710615?_encoding=UTF8&pd_rd_i=B07S58MHXF&pd_rd_r=bdc4f451-eab4-4f00-b7ef-c44aeaceb57b&pd_rd_w=YOqqT&pd_rd_wg=xwLZW&pf_rd_p=016ff402-c614-45c8-b1eb-3e2e77f84f7c&pf_rd_r=1198CHGY3TT6TASMM8MX&psc=1&refRID=1198CHGY3TT6TASMM8MX', 2000)
+computer = Page('https://www.amazon.com/Apple-MacBook-16-inch-512GB-Storage/dp/B081FZV45H/ref=sr_1_4?crid=30VE2VXC6RFMW&keywords=apple+macbook+pro&qid=1578025208&s=electronics&sprefix=apple+mac%2Celectronics%2C193&sr=1-4', 2000)
 pages.append(computer)
 adapter = Page('https://www.amazon.com/Belkin-Charge-Rockstar-Adapter-Charging/dp/B074WDWVX1/ref=sr_1_3?crid=30PHZFKKYQDQE&keywords=lightning+to+headphone+jack+belkin&qid=1576480494&s=electronics&sprefix=lightning+to+head%2Celectronics%2C159&sr=1-3',10)
 pages.append(adapter)
